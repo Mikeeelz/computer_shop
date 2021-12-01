c30f3ebb9fd5e54d882f1b84cb610f43eb7e68a9
Mihail Korotaev <misha@DESKTOP-C4VN1I3.localdomain>
2021-12-01 20:35:51 +0300

    add composer

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo 'Hello world!';
+require_once '../vendor/autoload.php';
+
+$helloWorld = new \App\HelloWorld();
+
+$helloWorld->hello();
---
+++ src/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class HelloWorld
+{
+    public function hello(): void
+    {
+        echo 'Hello world';
+    }
+}
