0d249a743b46db5b6abb4a6f446c7e853f59328d
Mihail Korotaev <misha@DESKTOP-C4VN1I3.localdomain>
2022-02-20 21:09:47 +0300

    product with category

--- src/Controller/Admin/AdminProductContr.php
+++ src/Controller/Admin/AdminProductContr.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Model\Category;
 use App\Model\Product;
 use App\Twig;
 
@@ -16,12 +17,15 @@ class AdminProductContr
 
     public function create(): void
     {
-        Twig::render('admin/catalog/product/create.html.twig');
+        Twig::render('admin/catalog/product/create.html.twig', [
+            'categories' => Category::findAll(),
+        ]);
     }
 
     public function save(): void
     {
-        $product = new Product($_POST['title'], $_POST['price']);
+        $product = new Product($_POST['title'], $_POST['price'], $this->saveImage());
+        $product->categoryId = $_POST['categoryId'];
         $product->save();
 
         header('Location: http://localhost/admin/product');
@@ -31,16 +35,54 @@ class AdminProductContr
     {
         Twig::render('admin/catalog/product/edit.html.twig', [
             'product' => Product::findById($id),
+            'categories' => Category::findAll(),
         ]);
     }
 
-    public function update(int $id ): void
+    public function update(int $id): void
     {
         $product = Product::findById($id);
         $product->title = $_POST['title'];
-        $product->price = $_POST['price'];
+        $product->price = (int)$_POST['price'];
+        $product->categoryId = (int)$_POST['categoryId'];
+
+        $image = $this->saveImage();
+
+        if ($image !== null) {
+            $this->deleteImage($product->image);
+            $product->image = $image;
+        }
+
         $product->save();
 
         header('Location: http://localhost/admin/product');
+    }
+
+    private function saveImage(): ?string
+    {
+        $fileData = $_FILES['image'];
+
+        if ($fileData['error'] !== 0) {
+            return null;
+        }
+
+        $extension = explode('/', $fileData['type']);
+        $fileName = 'product_' . time() . '.' . $extension[1];
+        $filePath = '/upload/' . $fileName;
+
+        if (copy($fileData['tmp_name'], __DIR__ . '/../../../public' . $filePath)) {
+            unlink($fileData['tmp_name']);
+        }
+
+        return $filePath;
+    }
+
+    private function deleteImage(string $image): void
+    {
+        $filePath = __DIR__ . '/../../../public' . $image;
+
+        if (file_exists($filePath)) {
+            unlink($filePath);
+        }
     }
 }
--- src/Model/Product.php
+++ src/Model/Product.php
@@ -9,11 +9,18 @@ class Product
     public ?int $id = null;
     public string $title;
     public int $price;
+    public string $image;
+    public ?int $categoryId = null;
 
-    public function __construct(string $title, ?int $id = null, int $price)
-    {
+    public function __construct(
+        string $title,
+        int $price,
+        string $image,
+        ?int $id = null
+    ) {
         $this->title = $title;
         $this->id = $id;
+        $this->image = $image;
         $this->price = $price;
     }
 
@@ -21,12 +28,15 @@ class Product
     {
         $connection = DB::getConnection();
 
-        $query = $connection->query('select id, title, price from product');
+        $query = $connection->query('select * from product');
 
         $result = [];
 
         foreach ($query->fetchAll(\PDO::FETCH_ASSOC) as $product) {
-            $result[] = new Product($product['title'], $product['id'], $product['price']);
+            $item = new Product($product['title'], $product['price'], $product['image'], $product['id']);
+            $item->categoryId = $product['category_id'];
+
+            $result[] = $item;
         }
 
         return $result;
@@ -34,32 +44,43 @@ class Product
 
     public static function findById(int $id): Product
     {
-        $sql = 'select id, title, price from product where id = :id';
+        $sql = 'select * from product where id = :id';
         $connection = DB::getConnection();
         $query = $connection->prepare($sql);
         $query->bindParam('id', $id);
         $query->execute();
         $product = $query->fetch(\PDO::FETCH_ASSOC);
 
-        return new Product($product['title'], $product['id'], $product['price']);
+        $result = new Product($product['title'], $product['price'], $product['image'], $product['id']);
+        $result->categoryId = $product['category_id'];
+
+        return $result;
     }
 
     public function save(): void
     {
         if ($this->id === null) {
-            $sql = 'insert into product (title,price) values (:title, :price)';
+            $sql = 'insert into product (title,price, image, category_id) values (:title, :price, :image, :category_id)';
         } else {
-            $sql = 'update product set title = :title, price = :price where id = :id';
+            $sql = 'update product set title = :title, price = :price, image=:image, category_id=:category_id where id = :id';
         }
 
         $connection = DB::getConnection();
         $query = $connection->prepare($sql);
         $query->bindParam('title', $this->title);
         $query->bindParam('price', $this->price);
+        $query->bindParam('image', $this->image);
+        $query->bindParam('category_id', $this->categoryId);
+
         if ($this->id !== null) {
             $query->bindParam('id', $this->id);
         }
 
         $query->execute();
+    }
+
+    public function getCategory(): Category
+    {
+        return Category::findById($this->categoryId);
     }
 }
